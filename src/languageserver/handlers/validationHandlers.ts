701d94ada9dc1c5e53d5abe1a1a265fe2bbff43f
Joao Vitor <joaovitorsrs7@gmail.com>
2022-05-19 10:54:01 -0300

    feat: now the validation is working and it's using the chars setting
    still is necessary to add some more configurations and add support to ignore some special cases

--- src/languageserver/handlers/validationHandlers.ts
+++ src/languageserver/handlers/validationHandlers.ts
@@ -6,13 +6,25 @@ import {
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { SettingsState } from '../../stupidSettings';
 
+interface charDiagnosticError {
+  char: string;
+  start: number;
+  end: number;
+}
+
 export class ValidationHandler {
   private stupidSettings: SettingsState;
+
+  private closeChars: string[];
+  private openChars: string[];
+
   constructor(
     private readonly connection: Connection,
     settingsLanguage: SettingsState
   ) {
     this.stupidSettings = settingsLanguage;
+    this.closeChars = this.returnCloseChars();
+    this.openChars = this.returnOpenChars();
 
     this.stupidSettings.documents.onDidChangeContent((change) => {
       this.validate(change.document);
@@ -26,24 +38,69 @@ export class ValidationHandler {
     });
   }
 
+  private returnCloseChars(): string[] {
+    const closeChars: string[] = [];
+    this.stupidSettings.charsComparassion.forEach((element) => {
+      closeChars.push(element[1]);
+    });
+    return closeChars;
+  }
+
+  private returnOpenChars() {
+    const openChars: string[] = [];
+    this.stupidSettings.charsComparassion.forEach((element) => {
+      openChars.push(element[0]);
+    });
+    return openChars;
+  }
+
+  private returnConntraryChar(char: string): string | null {
+    let result = '';
+    this.stupidSettings.charsComparassion.forEach((element) => {
+      if (element[1] == char) result = element[0];
+      else if (element[0] == char) result = element[1];
+    });
+    return result;
+  }
+
+  private returnErrorsFromTxt(txt: string): charDiagnosticError[] {
+    const depthChars: charDiagnosticError[] = [];
+
+    for (let i = 0; i <= txt.length; i++) {
+      if (depthChars.length >= this.stupidSettings.maxErrorsComputed) break;
+
+      if (this.openChars.includes(txt[i])) {
+        depthChars.push({ start: i, end: i + txt[i].length, char: txt[i] });
+      } else if (
+        this.closeChars.includes(txt[i]) &&
+        depthChars[depthChars.length - 1].char ==
+          this.returnConntraryChar(txt[i])
+      ) {
+        depthChars.pop();
+      }
+    }
+    return depthChars;
+  }
+
   validate(textDocument: TextDocument): void {
-    const text = textDocument.getText();
-    const patterr = /\b[A-Z]{2,}\b/g;
-    let m: RegExpExecArray | null;
+    const txt = textDocument.getText();
 
     const diagnostics: Diagnostic[] = [];
-    while ((m = patterr.exec(text))) {
+
+    const parenthesPositions = this.returnErrorsFromTxt(txt);
+
+    parenthesPositions.forEach((position) => {
       const diagnostic: Diagnostic = {
         severity: DiagnosticSeverity.Warning,
         range: {
-          start: textDocument.positionAt(m.index),
-          end: textDocument.positionAt(m.index + m[0].length),
+          start: textDocument.positionAt(position.start),
+          end: textDocument.positionAt(position.end),
         },
-        message: `${m[0]} is all uppercase.`,
+        message: 'JUST CLOSES THIS SH!#',
         source: 'ex',
       };
       diagnostics.push(diagnostic);
-    }
+    });
 
     this.connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
   }
