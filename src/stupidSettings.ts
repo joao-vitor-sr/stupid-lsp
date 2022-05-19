39f74bb7fc7dc1fd15bcd9227f0020aec31972a5
Joao Vitor <joaovitorsrs7@gmail.com>
2022-05-18 21:31:05 -0300

    feat: new setting that handles maximum erros
    this setting is essential for performance, to dont get returning errors forever

--- src/stupidSettings.ts
+++ src/stupidSettings.ts
@@ -1,9 +1,15 @@
-import { TextDocuments } from 'vscode-languageserver/node';
+import { TextDocuments, ClientCapabilities } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 
 export class SettingsState {
+  constructor() {
+    this.maxErrorsComputed = 5000;
+  }
+
   documents: TextDocuments<TextDocument> | TextDocumentTestManager =
     new TextDocuments(TextDocument);
+  capabilities: ClientCapabilities | undefined;
+  maxErrorsComputed: number;
 }
 
 export class TextDocumentTestManager extends TextDocuments<TextDocument> {
