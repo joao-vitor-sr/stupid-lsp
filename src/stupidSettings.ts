0e75f4d5a8047306a2895eb8b94447c03f951e5a
Joao Vitor <joaovitorsrs7@gmail.com>
2022-05-19 10:53:16 -0300

    feat: now the settings has a setting that holds the chars for comparassion
    those chars are used for comparassion, the validation will use those

--- src/stupidSettings.ts
+++ src/stupidSettings.ts
@@ -4,12 +4,14 @@ import { TextDocument } from 'vscode-languageserver-textdocument';
 export class SettingsState {
   constructor() {
     this.maxErrorsComputed = 5000;
+    this.charsComparassion = [['(', ')'], ['[', ']']];
   }
 
   documents: TextDocuments<TextDocument> | TextDocumentTestManager =
     new TextDocuments(TextDocument);
   capabilities: ClientCapabilities | undefined;
   maxErrorsComputed: number;
+  charsComparassion: string[][];
 }
 
 export class TextDocumentTestManager extends TextDocuments<TextDocument> {
