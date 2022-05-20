308071bf3407e246c91c31d1e9db8f9d146c7b4a
Joao Vitor <joaovitorsrs7@gmail.com>
2022-05-20 14:52:58 -0300

    style: the prettier was applied at the stupidSettings file

--- src/stupidSettings.ts
+++ src/stupidSettings.ts
@@ -4,30 +4,16 @@ import { TextDocument } from 'vscode-languageserver-textdocument';
 export class SettingsState {
   constructor() {
     this.maxErrorsComputed = 5000;
-    this.charsComparassion = [['(', ')'], ['[', ']']];
+    this.charsComparassion = [
+      ['(', ')'],
+      ['[', ']'],
+    ];
     this.disableBadLanguage = false;
   }
 
-  documents: TextDocuments<TextDocument> | TextDocumentTestManager =
-    new TextDocuments(TextDocument);
+  documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
   capabilities: ClientCapabilities | undefined;
   maxErrorsComputed: number;
   charsComparassion: string[][];
   disableBadLanguage: boolean;
-}
-
-export class TextDocumentTestManager extends TextDocuments<TextDocument> {
-  testTextDocuments = new Map<string, TextDocument>();
-
-  constructor() {
-    super(TextDocument);
-  }
-
-  get(uri: string): TextDocument | undefined {
-    return this.testTextDocuments.get(uri);
-  }
-
-  set(textDocument: TextDocument): void {
-    this.testTextDocuments.set(textDocument.uri, textDocument);
-  }
 }
